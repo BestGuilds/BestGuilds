f2fddfd624ae56ac2736d5f43f3e6535883052ec
Xierip <xierip@gmail.com>
2018-10-03 17:41:27 +0200

    fix

--- api/src/main/java/pl/bestguilds/api/BestGuildsAPI.java
+++ api/src/main/java/pl/bestguilds/api/BestGuildsAPI.java
@@ -1,11 +1,11 @@
 package pl.bestguilds.api;
 
-import java.io.File;
 import org.jetbrains.annotations.NotNull;
 import pl.bestguilds.api.command.CommandManager;
 import pl.bestguilds.api.guild.GuildManager;
-import pl.bestguilds.api.storage.Storage;
 import pl.bestguilds.api.user.UserManager;
+
+import java.io.File;
 
 public interface BestGuildsAPI {
 
